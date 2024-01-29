9c0e580e8ae39b0067722c85e429300eac84647b
Ishana0 <ishanakalikiri01@gmailcom>
2024-01-29 17:01:46 +0530

    carousel-hooks

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,46 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [currImageIndex, setCurrImageIndex] = useState(0);
+
+  const prev = () => {
+    setCurrImageIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : images.length - 1
+    );
+  };
+
+  const next = () => {
+    setCurrImageIndex((prevIndex) =>
+      prevIndex < images.length - 1 ? prevIndex + 1 : 0
+    );
+  };
+
+  return (
+    <>
+      <div className="main-container">
+        <div className="btn-container">
+          <div>
+            <button className="left-btn" onClick={prev}>
+              <ArrowBackIosIcon style={{ fill: "white" }} />
+            </button>
+          </div>
+          <div>
+            <button className="right-btn" onClick={next}>
+              <ArrowForwardIosIcon style={{ fill: "white" }} />
+            </button>
+          </div>
+        </div>
+        <div className="image-span-container">
+          <span className="text title">{images[currImageIndex].title}</span>
+          <img
+            src={images[currImageIndex].img}
+            alt={images[currImageIndex].title}
+          />
+          <span className="text sub-title">{images[currImageIndex].subtitle}</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Carousel;
